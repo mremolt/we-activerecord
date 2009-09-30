6efd547f23fa4fd078afb54e403ae9febf949be2
Marc Remolt <marc.remolt@googlemail.com>
2009-09-30 14:17:24 +0200

    more phpdocs for Benutzer

--- models/Benutzer.php
+++ models/Benutzer.php
@@ -18,61 +18,122 @@ class Benutzer extends \library\ActiveRecord
     protected $registriert_seit;
     protected $anrede;
 
+    /**
+     * Getter 
+     * 
+     * @return string
+     */
     public function getVorname()
     {
         return $this->vorname;
     }
 
+    /**
+     * Setter
+     *
+     * @param string $vorname
+     * @return Benutzer
+     */
     public function setVorname($vorname)
     {
         $this->vorname = $vorname;
         return $this;
     }
 
+    /**
+     * Getter
+     *
+     * @return string
+     */
     public function getName()
     {
         return $this->name;
     }
 
+    /**
+     * Setter
+     *
+     * @param string $name
+     * @return Benutzer
+     */
     public function setName($name)
     {
         $this->name = $name;
         return $this;
     }
 
+    /**
+     * Getter
+     *
+     * @return string
+     */
     public function getEmail()
     {
         return $this->email;
     }
 
+    /**
+     * Setter
+     *
+     * @param string $email
+     * @return Benutzer
+     */
     public function setEmail($email)
     {
         $this->email = $email;
         return $this;
     }
 
+    /**
+     * Setter
+     *
+     * @param string $passwort
+     * @return Benutzer
+     */
     public function setPasswort($passwort)
     {
         $this->passwort = $passwort;
         return $this;
     }
 
+    /**
+     * Getter
+     *
+     * @return string
+     */
     public function getRegistriert_seit()
     {
         return $this->registriert_seit;
     }
 
+    /**
+     * Setter
+     *
+     * @param string $registriert_seit
+     * @return Benutzer
+     */
     public function setRegistriert_seit($registriert_seit)
     {
         $this->registriert_seit = $registriert_seit;
         return $this;
     }
 
+    /**
+     * Getter
+     *
+     * @return string
+     */
     public function getAnrede()
     {
         return $this->anrede;
     }
 
+    /**
+     * Setter
+     *
+     * @param string $anrede
+     * @return Benutzer
+     */
     public function setAnrede($anrede)
     {
         $this->anrede = $anrede;
@@ -84,11 +145,22 @@ class Benutzer extends \library\ActiveRecord
         return $this->passwort === $passwort;
     }
 
+    /**
+     * Getter
+     *
+     * @return array
+     */
     public function getSeminartetmine()
     {
         return Seminartermin::findByBenutzer($this);
     }
 
+    /**
+     * Fügt dem Benutzer einen neue Teilahme an einem Seminartermin hinzu.
+     *
+     * @param Seminartermin $seminartetmin
+     * @return Benutzer
+     */
     public function addSeminartermin(Seminartermin $seminartetmin)
     {
         // wir können die Zwischentabelle nicht befüllen, so lange der Seminartermin
@@ -121,6 +193,15 @@ class Benutzer extends \library\ActiveRecord
         ));
     }
 
+    /**
+     * Findet Benutzer eines Seminartermins über die Zwischentabelle nimmt_teil
+     *
+     * Hier ist ein SQL JOIN notwendig, wobei die Spalten der Zwischentabelle nicht
+     * im Objekt auftauchen sollen, also kein SELECT *.
+     *
+     * @param Seminartermin $seminartermin
+     * @return array
+     */
     public static function findBySeminartermin(Seminartermin $seminartermin)
     {
         $sql = sprintf(
